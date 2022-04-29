f65202952cc3a41e7670f61141cd9dde9654baf1
Kim-in-ju <jmmj1991@hanmail.net>
2022-04-29 11:34:05 +0900

    first commit

--- ju/ju/urls.py
+++ ju/ju/urls.py
@@ -1,4 +1,4 @@
-"""test3333 URL Configuration
+"""ju URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- ju/ju/wsgi.py
+++ ju/ju/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for test3333 project.
+WSGI config for ju project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test3333.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ju.settings')
 
 application = get_wsgi_application()
